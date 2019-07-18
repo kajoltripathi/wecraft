773521cbd0bc7038fca19231b8473e754da62829
kajoltripathi <kaju9323@gmail.com>
2019-07-18 17:14:25 +0530

    add files

---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+      <meta charset="UTF-8">
+      <title>We Change</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+   </head>
+
+   <body>
+      <nav class="navbar navbar-expand-lg nav-second nav-m ">
+         <div class="container">
+            <h3 class="heading">Shapely Demo</h3>
+            <ul class="nav justify-content-end">
+               <li class="nav-item ">
+                  <a class="nav-link active page-1" href="#">HOME</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link page-1" href="#">BLOG</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link page-1" href="#">PORTFOLIO</a>
+               </li>
+               <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle page-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     ABOUT THE TESTS
+                  </a>
+                  <div class="dropdown-menu border-0 shadow">
+                     <a class="dropdown-item" href="#">Page image alignment</a>
+                     <a class="dropdown-item" href="#">page markup and<br>Formatting </a>
+                     <a class="dropdown-item" href="#">Clear Floats</a>
+                     <a class="dropdown-item" href="#">Page with comments </a>
+                     <a class="dropdown-item" href="#">Page with comments disabled</a>
+               </li>
+               <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle page-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     LEVEL 1
+                  </a>
+                  <div class="dropdown-menu border-0 shadow">
+                     <a class="dropdown-item" href="#">Level 2 </a>
+                     <a class="dropdown-item" href="#"> Level 2a</a>
+                     <a class="dropdown-item" href="#">Level 2b</a>
+               </li>
+               <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle page-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     SHOP
+                  </a>
+                  <div class="dropdown-menu border-0 shadow">
+                     <a class="dropdown-item" href="#">Shop A</a>
+                     <a class="dropdown-item" href="#"> Shop B</a>
+               </li>
+            </ul>
+         </div>
+      </nav>
+   </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,120 @@
+<?php require_once 'header.php';?>
+<section class="index-section " style="height: 100vh;">
+	<div class="container">
+		<div class="row justify-content-center ">
+			<div class="col-12 col-sm-12">
+				<h1 class="sub-heading">We Change Everything<br> WordPress</h1>
+				<p>This is the only WordPress theme you will ever want to use.</p>
+				<button type="button" class="btn btn-primary btn-lg btn-two button-p">READ MORE</button>
+				<button type="button" class="btn btn-outline-primary btn-lg btn-two btn-t ">DOWNLOAD NOW</button>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-2 py-5">
+	<div class="container">
+		<div class="row row-seo">
+			<div class="col-12 col-sm-7">
+				<img src="assets/images/ipad.jfif" alt="ipad" style="width:100%;height:300px;">
+			</div>
+			<div class="col-12 col-sm-5 seo">
+				<h2 class="heading-3"> SEO Friendly</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+					Nam pulvinar luctus sem, eget porta orci. Maecenas<br>
+					molestie dui id diam feugiat, eu tincidunt mauris<br>
+					aliquam. Duis commodo vitae ligula et interdum.<br>
+					Maecenas faucibus mattis imperdiet. In rhoncus ac<br> ligula id ultricies.</p>
+				<button type="button" class="btn btn-primary btn-lg btn-two button-p">READ MORE</button>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-3 py-5">
+	<div class="container">
+		<div class="row port">
+			<div class="col-12 col-sm-7 ">
+				<h2 class="heading-3">Portfolio Section</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+					Nam pulvinar luctus sem, eget porta orci. Maecenas<br>
+					molestie dui id diam feugiat, eu tincidunt mauris <br>
+					aliquam. Duis commodo vitae ligula et interdum.</p>
+				<button type="button" class="btn btn-primary btn-lg btn-two button-p ">READ MORE</button>
+			</div>
+			<div class="col-12 col-sm-5">
+				<img class="img1" src="assets/images/ipad-105.jpg" alt="105" style="width:100%;height:300px;">
+			</div>
+		</div>
+	</div>
+</section>
+<section class="small-section py-5" style="height: 60vh;">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-12 col-12">
+				<h1 class="sub-heading sub-heading1 ">Small Parallax Section</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet rhoncus porta. Ut quis sem quis purus lobortis dictum. Aliquam nec<br>
+					dignissim nisl. Vivamus cursus feugiat sapien, eget tincidunt leo ornare quis.</p>
+				<button type="button" class="btn btn-primary btn-lg btn-two button-p btn-r"> MORE INFO</button>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-4">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-12 col-12">
+				<h2>Limitless Options</h2>
+				<p>Phasellus sed nisi ac dui interdum semper. Etiam consequat fermentum sollicitudin. Fusce vulputate porta faucibus. Vivamus nulla tellus,<br>
+					accumsan non efficitur id, pretium quis ante</p>
+				<button type="button" class="btn btn-primary btn-lg btn-two">DOWNLOAD NOW </button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-sm-12">
+				<img class="imgs" src="assets/images/pexels.jpeg" alt="pexels" style="width: 100%">
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-5 " style="height: 100vh;">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-12 col-sm-12">
+				<h2>Our Latest Projects</h2>
+				<p>Here is our latest projects. You'll love them!</p>
+				<img class="images6" src="assets/images/1.jpg" style="width: auto;height:250px;">
+				<img class="images6" src="assets/images/2.jpg" style="width: auto;height:250px;">
+				<img class="images6" src="assets/images/3.jpg" style="width: auto;height:250px;">
+				<img class="images6" src="assets/images/4.jpg" style="width: auto;height:250px;">
+				<img class="images6" src="assets/images/5.jpg" style="width: auto;height:250px;">
+				<img class="images6" src="assets/images/6.jpg" style="width: auto;height:250px;">
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-6">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-12">
+				<h2>What Our Customers Say</h2>
+				<p>Nunc sit amet lobortis nulla. Nunc ullamcorper, mi id luctus dictum, augue tortor dictum ipsum, nec congue arcu <br>
+					lorem in nisl. Duis neque lacus, viverra non mauris ac, pharetra rhoncus libero. Aliquam varius viverra ex, in venenatis<br>
+					magna ornare sit amet. Integer varius sit amet turpis eu ullamcorper.</p>
+				<img class="image-a" src="assets/images/aigars.jpg">
+				<p>Aigars Silkalns</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class=" section-7">
+	<div class=container>
+		<div class="row awsm">
+			<div class="col-12 col-sm-9">
+				<h2>Do you like this awesome WordPress theme?</h2>
+			</div>
+			<div class="col-12 col-sm-3">
+				<button type="button" class="btn btn-primary btn-lg btn-two">DOWNLOAD NOW </button>
+			</div>
+		</div>
+	</div>
+</section>
+<?php require_once 'footer.php';?>
